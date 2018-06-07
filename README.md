2088e31c517d55788f05e8fa899a581d87cc0e3e
uojo <friend1020@163.com>
2018-06-07 16:05:02 +0800

    v1.1.1

--- README.md
+++ README.md
@@ -174,6 +174,8 @@ commit(Types.M_MOD_SET,{
 ```
 
 ## ChangeLog
+### 1.1.1
+- mods.js 新增 createEntity 方法，用于生成默认实体结构
 ### 1.1.0
 - 删除 package.json 内 module 字段
 ### 1.0.3
--- dist/mods.js
+++ dist/mods.js
@@ -1,11 +1,37 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 var list = exports.list = {
-	items: [],
-	pageBean: { totalCount: 0, pageSize: 10, pageNo: 1 },
-	errorMessage: "",
-	itemsStep: ""
+  items: [],
+  pageBean: { totalCount: 0, pageSize: 10, pageNo: 1 },
+  errorMessage: "",
+  itemsStep: ""
+};
+
+var createEntity = exports.createEntity = function createEntity() {
+  var type = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'single';
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+  var rlt = void 0;
+  if (/single|other/.test(type)) {
+    rlt = {
+      errorMessage: '',
+      step: ''
+    };
+    rlt.data = options.data || {};
+  } else if (type === 'list') {
+    rlt = {
+      items: [],
+      pageBean: { totalCount: 0, pageSize: 10, pageNo: 1 },
+      errorMessage: '',
+      itemsStep: ''
+    };
+  }
+
+  rlt.type = type;
+  rlt.source = 'capsule';
+
+  return rlt;
 };
